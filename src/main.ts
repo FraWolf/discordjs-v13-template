739f1a407220207e072b90ceebf8e889c83f0c54
Francesco <frawolf@outlook.it>
2022-02-06 11:36:54 +0100

    fix: name exported from env

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@
 
 require("dotenv").config();
 
-import BotClient from "./core/Client";
+import BotClient from "./core/client";
 import { Intents } from "discord.js";
-const { TOKEN, OWNER_USER_ID, ADMINS_ID } = process.env;
+const { TOKEN, OWNER_ID, ADMINS_ID } = process.env;
 
 //----------------------------------------------------------------------------------------------
 // Classes
 
 const client = new BotClient({
-  ownerId: OWNER_USER_ID!,
+  ownerId: OWNER_ID!,
   admins: ADMINS_ID!,
   intents: [Intents.FLAGS.GUILD_MESSAGES],
 });
